5425281d48ae0825313d31ab8917f86f233596ba
jonjeden <jonjeden@gmail.com>
2016-10-16 21:32:24 -0700

    new style to blog

--- blog/models.py
+++ blog/models.py
@@ -9,11 +9,34 @@ class Post(models.Model):
 	created_date = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True, null=True)
 	
-	#method to actually post the new Blog Post. When you create a post,
-	#it isn't auto published (a good thing)
+	#method to actually post the new draft post. When you create a post,
+	#it isn't auto published (a good thing).
 	def publish(self):
 		self.published_date = timezone.now()
 		self.save()
 		
+		
+	def approved_comments(self):
+		return self.comments.filter(approved_comment=True)
+		
 	def __str__(self):
 		return self.title
+		
+class Comment(models.Model):
+	#related name allows us to have access comments from a Post model
+	post = models.ForeignKey('blog.Post', related_name='comments')
+	author = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	approved_comment = models.BooleanField(default=False)
+	
+	#method to allow/show a user comment. must be approved.
+	def approve(self):
+		self.approved_comment = True
+		self.save()
+		
+	def __str__(self):
+		return self.text
+		
+		
+
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+#Create your views here.
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post': post})
 	
+def search_post_list(request):
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+	return render(request, 'blog/post_list.html', {'posts': posts})
+	
+def blog_search_list(request):
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+	return render(request, 'blog/post_list.html', {'posts': posts})
+
 @login_required
 def post_new(request):
 	#handle blank form and a filled out blog form/post that was submitted
@@ -65,3 +73,31 @@ def post_remove(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	post.delete()
 	return redirect('post_list')
+	
+def add_comment_to_post(request, pk):
+	#get post (if exists) first
+	post = get_object_or_404(Post, pk=pk)
+	#if submitting a comment, do the following:
+	if request.method == "POST":
+		form = CommentForm(request.POST) #get comment form
+		if form.is_valid():
+			comment = form.save(commit=False)
+			comment.post = post
+			comment.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = CommentForm() #not posting, but blank form on load
+	return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+	comment = get_object_or_404(Comment, pk=pk)
+	comment.approve()
+	return redirect('post_detail', pk=comment.post.pk)
+	
+@login_required
+def comment_remove(request, pk):
+	comment = get_object_or_404(request, pk)
+	post_pk = comment.post.pk
+	comment.delete()
+	return redirect('post_detail', pk=post_pk)
